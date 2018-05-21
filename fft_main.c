0980be4db2316a5d5e174ab5d854b96a307337a5
yangchengwei <peter840706@gmail.com>
2018-05-21 11:06:11 +0800

    midterm exam

--- fft_main.c
+++ fft_main.c
@@ -1,67 +1,115 @@
 #include <time.h>
 #include "fast_fourier_transform.c"
 
-#define DEBUG_OTHER 1
-#define PRINT_RESULT 10
+#define DEBUG_OTHER 0
+#define PRINT_RESULT 1
 
 int main()
 {
-	int i, N = (int)(pow(2,6)*pow(3,5)*pow(5,5));
-	clock_t t1, t2;
+	int i, j, N;
 	double *x_re, *x_im;
+	clock_t t1, t2;
 	
-	x_re = (double *) malloc(N*sizeof(double));
-	x_im = (double *) malloc(N*sizeof(double));
+	printf("N=");
+	scanf("%d",&N);
+	printf("N=%d\n",N);
 	
-	
-	/* main */
-	
-	for(i=0;i<N;++i)
+	if (N==-1){
+		int *N_array;
+		
+		N_array = (int *) malloc(7*sizeof(double));
+		N_array[0]=16777216;
+		N_array[1]=14348907;
+		N_array[2]=48828125;
+		N_array[3]=10077696;
+		N_array[4]=11390625;
+		N_array[5]=10000000;
+		N_array[6]=10935000;
+		
+		for(j=0;j<7;j++)
+		{
+			N=N_array[j];
+			printf("N=%d\n",N);
+			
+			x_re = (double *) malloc(N*sizeof(double));
+			x_im = (double *) malloc(N*sizeof(double));
+			
+			for(i=0;i<N;++i)
+			{
+				x_re[i] = i;
+				x_im[i] = 0.0;
+			}
+			
+			t1 = clock();
+			
+			FFT_general(x_re, x_im, N);
+			
+			t2 = clock();
+			
+			printf("time = %f\n",1.0*(t2-t1)/(double) CLOCKS_PER_SEC);
+			
+			free(x_re);
+			free(x_im);
+		}
+	}
+	else
 	{
-		x_re[i] = i;
-		x_im[i] = 0.0;
+		x_re = (double *) malloc(N*sizeof(double));
+		x_im = (double *) malloc(N*sizeof(double));
+		
+		/* main */
+		
+		for(i=0;i<N;++i)
+		{
+			x_re[i] = i;
+			x_im[i] = 0.0;
+		}
+		
+		t1 = clock();
+		
+		FFT_general(x_re, x_im, N);
+		
+		t2 = clock();
+		
+		printf("time = %f\n",1.0*(t2-t1)/(double) CLOCKS_PER_SEC);
+		
+		#if PRINT_RESULT
+		system("pause");
+		for(i=0;i<N;i++)
+		{
+			printf("%f + %f i\n", x_re[i], x_im[i]);
+		}
+		#endif
+		
+		/* other */
+		
+		#if DEBUG_OTHER 
+		
+		for(i=0;i<N;++i)
+		{
+			x_re[i] = i;
+			x_im[i] = 0.0;
+		}
+		
+		t1 = clock();
+		
+		FFT_general_np_separated(x_re, x_im, N);
+		
+		t2 = clock();
+		
+		printf("time = %f\n",1.0*(t2-t1)/(double) CLOCKS_PER_SEC);
+		
+		#if PRINT_RESULT
+		system("pause");
+		for(i=0;i<N;i++)
+		{
+			printf("%f + %f i\n", x_re[i], x_im[i]);
+		}
+		#endif
+		
+		#endif
 	}
 	
-	t1 = clock();
-	
-	FFT_general(x_re, x_im, N);
-	
-	t2 = clock();
-	
-	printf("time = %f\n",1.0*(t2-t1)/(double) CLOCKS_PER_SEC);
-	
-	#if PRINT_RESULT
-	for(i=0;i<N;i+=N/PRINT_RESULT)
-	{
-		printf("%f + %f i\n", x_re[i], x_im[i]);
-	}
-	#endif
-	
-	
-	/* other */
-	
-	#if DEBUG_OTHER 
-	for(i=0;i<N;++i)
-	{
-		x_re[i] = i;
-		x_im[i] = 0.0;
-	}
-	
-	t1 = clock();
-	
-	FFT_general_np_separated(x_re, x_im, N);
-	
-	t2 = clock();
-	
-	printf("time = %f\n",1.0*(t2-t1)/(double) CLOCKS_PER_SEC);
-	
-	#if PRINT_RESULT
-	for(i=0;i<N;i+=N/10)
-	{
-		printf("%f + %f i\n", x_re[i], x_im[i]);
-	}
-	#endif
-	#endif
 	return 0;
 }
 
